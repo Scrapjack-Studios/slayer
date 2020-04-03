3307a46db8c2884294c18a7b0c9f133ca95e467d
PopeRigby <poperigby@tuta.io>
2020-04-03 13:49:18 -0700

    Add pistol to weapon_table

---
+++ WeaponTable.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var weapon_table = [
+    {
+    "name":"pistol",
+    "damage":10,
+    "discharge_cooldown":0.1,
+    "reload_time":2,
+    "mag_capacity": 15,
+    "tota_ammo_capacity": 120,
+    "projectile_type": "bullet"
+    },
+]
