a9026fa4ce727d54d63dd0c2037fc12dd8d14ccd
jujubunicorn <39468196+jujubunicorn@users.noreply.github.com>
2020-04-22 15:41:44 -0700

    Change bullets to kinematic body
    Bullet richote

--- Bullet.gd
+++ Bullet.gd
@@ -1,7 +1,7 @@
-extends RigidBody2D
+extends KinematicBody2D
 
+var velocity = Vector2()
 export (int) var speed
-
 var damage
 
 
@@ -13,10 +13,22 @@ func start_at(pos, dir, type, dmg, lifetime):
     position = pos
     rotation = dir
     damage = dmg
-    linear_velocity = Vector2(speed, 0).rotated(dir)
+    velocity = Vector2(speed, 0).rotated(dir)
 
+
+
+func _physics_process(delta):
+    var collision = move_and_collide(velocity * delta)
+    if collision:
+        velocity = velocity.bounce(collision.normal)
+        if collision.collider.has_method("hit"):
+            collision.collider.hit()
+
+func _on_VisibilityNotifier2D_screen_exited():
+    queue_free()
+    
 func explode():
-    linear_velocity = Vector2(0, 0)
+    velocity = Vector2(0, 0)
     $Sprite.hide()
     $Explosion.show()
     $Explosion.play("smoke")
