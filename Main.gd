81f49886b81bc07584a47cd3ef45b76596c3b8fa
PopeRigby <poperigby@tuta.io>
2020-05-27 15:17:45 -0700

    Pause game when showing pause menu
    It also resumes the game when you press Esc while the game is currently
    paused

--- Main.gd
+++ Main.gd
@@ -1,7 +1,3 @@
 extends Node
 
 export (PackedScene) var Map
-
-func _process(_delta):
-    if Input.is_action_pressed("pause_menu"):
-        $CanvasLayer/PauseMenu/PopupMenu.popup()
--- menus/PauseMenu.gd
+++ menus/PauseMenu.gd
@@ -1,3 +1,9 @@
 extends Control
 
-
+func _process(_delta):
+    if Input.is_action_just_released("pause_menu") and get_tree().paused == false:
+        $PopupMenu.popup()
+        get_tree().paused = true
+    elif Input.is_action_just_released("pause_menu") and get_tree().paused == true:
+        $PopupMenu.hide()
+        get_tree().paused = false
