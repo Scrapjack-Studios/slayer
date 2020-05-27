5832dbbdbba8ee891fbff42faceb3276fcbd2bd4
PopeRigby <poperigby@tuta.io>
2020-05-27 15:50:35 -0700

    Esc key also resumes
    Esc will resume the game if it's pressed while the game is paused.

--- menus/PauseMenu.gd
+++ menus/PauseMenu.gd
@@ -4,6 +4,9 @@ func _process(_delta):
     if Input.is_action_just_released("pause_menu") and get_tree().paused == false:
         $PopupMenu.popup()
         get_tree().paused = true
+    elif Input.is_action_just_released("pause_menu") and get_tree().paused == true:
+        $PopupMenu.hide()
+        get_tree().paused = false
 
 func _on_Resume_pressed():
     $PopupMenu.hide()
