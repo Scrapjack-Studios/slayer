19cd0597fde8c6286e4af0a17ab67437edea7c67
PopeRigby <poperigby@tuta.io>
2020-05-29 13:51:43 -0700

    Make pause menu exclusive

--- menus/PauseMenu.gd
+++ menus/PauseMenu.gd
@@ -3,14 +3,22 @@ extends Control
 func _process(_delta):
     if Input.is_action_just_released("pause_menu") and get_tree().paused == false:
         $PopupMenu.popup()
-        get_tree().paused = true
+        pause_game()
     elif Input.is_action_just_released("pause_menu") and get_tree().paused == true:
         $PopupMenu.hide()
-        get_tree().paused = false
+        resume_game()
 
 func _on_Resume_pressed():
     $PopupMenu.hide()
-    get_tree().paused = false
+    resume_game()
 
 func _on_Quit_Desktop_pressed():
     get_tree().quit()
+    
+func pause_game():
+    get_tree().paused = true
+    # TODO: this has to be changed when multiplayer is implemented
+    
+func resume_game():
+    get_tree().paused = false
+    # TODO: this has to be changed when multiplayer is implemented
