c4b244a1e976e0f6ccf5d355eca130938052c815
PopeRigby <poperigby@tuta.io>
2020-06-02 12:11:48 -0700

    Load default settings if config.cfg doesn't exist

--- menus/OptionsMenu.gd
+++ menus/OptionsMenu.gd
@@ -1,11 +1,10 @@
 extends Control
 
 func _ready():
-    pass
-
+    _save_settings()
+            
 func _on_Exit_pressed():
     $Buttons.hide()
-
 
 func _on_Video_pressed():
     $VideoOptions.popup()
@@ -13,10 +12,32 @@ func _on_Video_pressed():
 func _on_Audio_pressed():
     $AudioOptions.popup()
 
-
 func _on_Video_Exit_pressed():
     $VideoOptions.hide()
 
-
 func _on_Audio_Exit_pressed():
     $AudioOptions.hide()
+
+var default_settings = {
+    "video_settings":
+    {
+        "vsync":false,
+        "fullscreen":false
+    },
+    "audio_settings":
+    {
+        "mute":false
+    }
+}
+
+func _save_settings():
+    var file = File.new()
+    if file.file_exists("user://config.cfg"):
+        print($VideoOptions/VBoxContainer/VSync.is_pressed())
+    elif not file.file_exists("user://config.cfg"):
+        file.open("user://config.cfg", File.WRITE)
+        for option_category in default_settings:
+            file.store_string("[" + option_category + "]\n")
+            for option in default_settings[option_category]:
+                file.store_string(option + " = ")
+                file.store_string(str(default_settings[option_category][option]).to_lower() + "\n")
