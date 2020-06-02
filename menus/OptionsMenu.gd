f4440c9b1103d5b89667b8fcec36dcd4ce11c11b
PopeRigby <poperigby@tuta.io>
2020-06-02 14:40:30 -0700

    Move reset functionality to its own func

--- menus/OptionsMenu.gd
+++ menus/OptionsMenu.gd
@@ -1,7 +1,12 @@
 extends Control
 
+var config = ConfigFile.new()
+var file = File.new()
+
 func _ready():
-    pass
+    # sets everything to default values if config.cfg doesn't exist
+    if not file.file_exists("user://config.cfg"):
+        _reset_settings()
             
 func _on_Exit_pressed():
     $Buttons.hide()
@@ -25,28 +30,21 @@ func _on_Audio_Exit_pressed():
 
 func _save_settings():
     # saves the current state of settings as specified in the ui to config.cfg
-    var config = ConfigFile.new()
-    var file = File.new()
-    if file.file_exists("user://config.cfg"):
-        config.load("user://config.cfg")
-        config.set_value("video", "vsync", $VideoOptions/VBoxContainer/VSync.is_pressed())
-        config.set_value("video", "fullscreen", $VideoOptions/VBoxContainer/Fullscreen.is_pressed())
-        config.save("user://config.cfg")
-    elif not file.file_exists("user://config.cfg"):
-        # sets everything to default values if config.cfg doesn't exist
-        config.set_value("video", "vsync", false)
-        config.set_value("video", "fullscreen", false)
-        config.set_value("audio", "mute", false)
-        config.save("user://config.cfg")
+    config.load("user://config.cfg")
+    config.set_value("video", "vsync", $VideoOptions/VBoxContainer/VSync.is_pressed())
+    config.set_value("video", "fullscreen", $VideoOptions/VBoxContainer/Fullscreen.is_pressed())
+    config.save("user://config.cfg")
         
 func _load_settings():
     # loads the values from config.cfg into the ui
     pass
     
 func _reset_settings():
-    # resets the values in the ui
-    # when _save_settings() is called, they also get reset in config.cfg
-    pass
+    # sets everything to default values
+    config.set_value("video", "vsync", false)
+    config.set_value("video", "fullscreen", false)
+    config.set_value("audio", "mute", false)
+    config.save("user://config.cfg")
     
 func _apply_settings():
     # actually applies the settings from config.cfg in-game
