10afe5c2cb05350022ca9a6a2a4e9d021ad09721
PopeRigby <poperigby@tuta.io>
2020-06-03 14:31:52 -0700

    Make audio options work

--- menus/OptionsMenu.gd
+++ menus/OptionsMenu.gd
@@ -46,15 +46,21 @@ func _on_Audio_Save_pressed():
 func _save_settings():
     # saves the current state of settings as specified in the ui to config.cfg
     config.load("user://config.cfg")
+    # video
     config.set_value("video", "vsync", $VideoOptions/VBoxContainer/VSync.is_pressed())
     config.set_value("video", "fullscreen", $VideoOptions/VBoxContainer/Fullscreen.is_pressed())
+    # audio
+    config.set_value("audio", "mute", $AudioOptions/VBoxContainer/Mute.is_pressed())
     config.save("user://config.cfg")
         
 func _load_settings():
     # loads the values from config.cfg into the ui
     config.load("user://config.cfg")
+    # video
     $VideoOptions/VBoxContainer/VSync.set_pressed(config.get_value("video", "vsync"))
     $VideoOptions/VBoxContainer/Fullscreen.set_pressed(config.get_value("video", "fullscreen"))
+    # audio
+    $AudioOptions/VBoxContainer/Mute.set_pressed(config.get_value("audio", "mute"))
     
 func _reset_settings():
     # sets everything to default values
@@ -68,9 +74,4 @@ func _apply_settings():
     config.load("user://config.cfg")
     OS.set_use_vsync(config.get_value("video", "vsync"))
     OS.set_window_fullscreen(config.get_value("video", "fullscreen"))
-
-
-
-
-
-
+    # TODO: make this mute everything. so put all sounds in a group and mute the group
