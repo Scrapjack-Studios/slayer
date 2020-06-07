fba1fe6c35cd7f282528d4a20b7f223b15b1a159
PopeRigby <poperigby@tuta.io>
2020-06-07 16:22:10 -0700

    Hook shooting range up to button

--- MainMenu.gd
+++ MainMenu.gd
@@ -6,3 +6,8 @@ func _ready():
 
 func _on_PlayButton_pressed():
     $PlayMenu.popup()
+
+
+func _on_ShootingRange_pressed():
+    # warning-ignore:return_value_discarded
+    get_tree().change_scene("res://maps/ShootingRange.tscn")
