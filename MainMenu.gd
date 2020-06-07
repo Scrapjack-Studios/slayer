a9e1304deafcf41d894c0e41f9c43280ae9a91e9
PopeRigby <poperigby@tuta.io>
2020-06-07 16:41:59 -0700

    Make options menu popup in correct place

--- MainMenu.gd
+++ MainMenu.gd
@@ -11,3 +11,9 @@ func _on_PlayButton_pressed():
 func _on_ShootingRange_pressed():
     # warning-ignore:return_value_discarded
     get_tree().change_scene("res://maps/ShootingRange.tscn")
+
+func _on_OptionsButton_pressed():
+    $OptionsMenu/Buttons.set_size(Vector2(959,869))
+    $OptionsMenu/Buttons.set_position(Vector2(105,103))
+#    $OptionsMenu/Buttons/Panel.hide()
+    $OptionsMenu/Buttons.popup()
