57c416035cb4e49378400248647a250973f5fbe3
PopeRigby <poperigby@tuta.io>
2020-06-08 13:05:36 -0700

    Popup instanced option menu

--- MainMenu.gd
+++ MainMenu.gd
@@ -11,7 +11,18 @@ func _on_ShootingRange_pressed():
     get_tree().change_scene("res://maps/ShootingRange.tscn")
 
 func _on_OptionsButton_pressed():
-    pass
+    $OptionsMenu/Buttons.set_position(Vector2(105,103))
+    $OptionsMenu/Buttons.set_size(Vector2(959,869))
+    $OptionsMenu/Buttons.anchor_left = 0.055
+    $OptionsMenu/Buttons.anchor_top = 0.102
+    $OptionsMenu/Buttons.anchor_right = 0.554
+    $OptionsMenu/Buttons.anchor_bottom = 0.902
+    $OptionsMenu/Buttons.margin_left = 0
+    $OptionsMenu/Buttons.margin_top = 0
+    $OptionsMenu/Buttons.margin_right = 0
+    $OptionsMenu/Buttons.margin_bottom = 0
+    $OptionsMenu/Buttons/Panel.hide()
+    $OptionsMenu/Buttons.popup()
 
 func _on_QuitButton_pressed():
     get_tree().quit()
