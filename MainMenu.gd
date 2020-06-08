30e974da50784960cb9eca46a228bfd330cae8da
PopeRigby <poperigby@tuta.io>
2020-06-08 12:51:04 -0700

    Add OptionsMenu.tscn as singleton

--- MainMenu.gd
+++ MainMenu.gd
@@ -3,19 +3,15 @@ extends Control
 func _ready():
     pass
 
-
 func _on_PlayButton_pressed():
     $PlayMenu.popup()
-
 
 func _on_ShootingRange_pressed():
     # warning-ignore:return_value_discarded
     get_tree().change_scene("res://maps/ShootingRange.tscn")
 
 func _on_OptionsButton_pressed():
-#    $OptionsMenu/Buttons.popup()
     pass
-
 
 func _on_QuitButton_pressed():
     get_tree().quit()
