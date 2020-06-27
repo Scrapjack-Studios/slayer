7171588982ae66b77642380968a1e2f1a40a1c35
PopeRigby <poperigby@tuta.io>
2020-06-27 14:44:50 -0700

    Add saving of resolution setting

--- menus/OptionsMenu.gd
+++ menus/OptionsMenu.gd
@@ -3,6 +3,8 @@ extends Control
 var config = ConfigFile.new()
 var file = File.new()
 var video_focused_before = true
+var string_splitter
+var selected_id
 
 func _ready():
     # checks if there are errors with config.cfg
@@ -12,7 +14,68 @@ func _ready():
     elif config.load("user://config.cfg") == 0:
         _load_settings()
         _apply_settings()
-      
+
+# configuration functions
+
+func _save_settings():
+    # saves the current state of settings as specified in the ui to config.cfg
+    config.load("user://config.cfg")
+    # video
+    config.set_value("video", "vsync", $VideoOptions/VBoxContainer/VSync.is_pressed())
+    config.set_value("video", "fullscreen", $VideoOptions/VBoxContainer/Fullscreen.is_pressed())
+    config.set_value("video", "fullscreen_crt", $VideoOptions/VBoxContainer/FullscreenCRT.is_pressed())
+    config.set_value("video", "fps_counter", $VideoOptions/VBoxContainer/FPSCounter.is_pressed())
+    
+    selected_id = get_node("VideoOptions/VBoxContainer/Resolution").get_selected_id()
+    string_splitter = get_node("VideoOptions/VBoxContainer/Resolution").get_item_text(selected_id).split("x")
+    config.set_value("video", "vid_width", int(string_splitter[0]))
+    config.set_value("video", "vid_height", int(string_splitter[1]))
+    
+    # audio
+    config.set_value("audio", "mute", $AudioOptions/VBoxContainer/Mute.is_pressed())
+    config.save("user://config.cfg")
+        
+func _load_settings():
+    # loads the values from config.cfg into the ui
+    config.load("user://config.cfg")
+    # video
+    $VideoOptions/VBoxContainer/VSync.set_pressed(config.get_value("video", "vsync"))
+    $VideoOptions/VBoxContainer/Fullscreen.set_pressed(config.get_value("video", "fullscreen"))
+    $VideoOptions/VBoxContainer/FullscreenCRT.set_pressed(config.get_value("video", "fullscreen_crt"))
+    $VideoOptions/VBoxContainer/FPSCounter.set_pressed(config.get_value("video", "fps_counter"))
+    for item in get_node("VideoOptions/VBoxContainer/Resolution").get_item_count():
+        string_splitter = get_node("VideoOptions/VBoxContainer/Resolution").get_item_text(item).split("x")
+        if int(string_splitter[0]) == config.get_value("video", "vid_width") && int(string_splitter[1]) == config.get_value("video", "vid_height"):
+            get_node("VideoOptions/VBoxContainer/Resolution").select(item)
+    # audio
+    $AudioOptions/VBoxContainer/Mute.set_pressed(config.get_value("audio", "mute"))
+    
+func _reset_settings():
+    # sets everything to default values
+    config.set_value("video", "vsync", false)
+    config.set_value("video", "fullscreen", false)
+    config.set_value("video", "fullscreen_crt", false)
+    config.set_value("video", "fps_counter", false)
+    config.set_value("video", "vid_width", 1920)
+    config.set_value("video", "vid_height", 1080)
+    config.set_value("audio", "mute", false)
+    config.save("user://config.cfg")
+    
+func _apply_settings():
+    # actually applies the settings from config.cfg in-game
+    config.load("user://config.cfg")
+    
+    OS.set_use_vsync(config.get_value("video", "vsync"))
+    
+    OS.set_window_fullscreen(config.get_value("video", "fullscreen"))
+    
+    get_node("/root/FullscreenCRT/ColorRect").visible = config.get_value("video", "fullscreen_crt")
+    if get_node("/root").has_node("MainMenu"):
+        get_node("/root/MainMenu/CanvasLayer/CRTShader").visible = not config.get_value("video", "fullscreen_crt")
+    
+    get_node("/root/FPSCounter/RichTextLabel").visible = config.get_value("video", "fps_counter")
+    # TODO: make a mute function that mutes everything in a 'sounds' group
+
 # buttons
             
 func _on_Exit_pressed():
@@ -62,58 +125,6 @@ func _on_VideoOptions_about_to_show():
 
 func _on_AudioOptions_about_to_show():
     $Buttons.hide()
-
-# configuration functions
-
-func _save_settings():
-    # saves the current state of settings as specified in the ui to config.cfg
-    config.load("user://config.cfg")
-    # video
-    config.set_value("video", "vsync", $VideoOptions/VBoxContainer/VSync.is_pressed())
-    config.set_value("video", "fullscreen", $VideoOptions/VBoxContainer/Fullscreen.is_pressed())
-    config.set_value("video", "fullscreen_crt", $VideoOptions/VBoxContainer/FullscreenCRT.is_pressed())
-    config.set_value("video", "fps_counter", $VideoOptions/VBoxContainer/FPSCounter.is_pressed())
-    # audio
-    config.set_value("audio", "mute", $AudioOptions/VBoxContainer/Mute.is_pressed())
-    config.save("user://config.cfg")
-        
-func _load_settings():
-    # loads the values from config.cfg into the ui
-    config.load("user://config.cfg")
-    # video
-    $VideoOptions/VBoxContainer/VSync.set_pressed(config.get_value("video", "vsync"))
-    $VideoOptions/VBoxContainer/Fullscreen.set_pressed(config.get_value("video", "fullscreen"))
-    $VideoOptions/VBoxContainer/FullscreenCRT.set_pressed(config.get_value("video", "fullscreen_crt"))
-    $VideoOptions/VBoxContainer/FPSCounter.set_pressed(config.get_value("video", "fps_counter"))
-    # audio
-    $AudioOptions/VBoxContainer/Mute.set_pressed(config.get_value("audio", "mute"))
-    
-func _reset_settings():
-    # sets everything to default values
-    config.set_value("video", "vsync", false)
-    config.set_value("video", "fullscreen", false)
-    config.set_value("video", "fullscreen_crt", false)
-    config.set_value("video", "fps_counter", false)
-    config.set_value("video", "vid_width", 1920)
-    config.set_value("video", "vid_height", 1080)
-    config.set_value("audio", "mute", false)
-    config.save("user://config.cfg")
-    
-func _apply_settings():
-    # actually applies the settings from config.cfg in-game
-    config.load("user://config.cfg")
-    
-    OS.set_use_vsync(config.get_value("video", "vsync"))
-    
-    OS.set_window_fullscreen(config.get_value("video", "fullscreen"))
-    
-    get_node("/root/FullscreenCRT/ColorRect").visible = config.get_value("video", "fullscreen_crt")
-    if get_node("/root").has_node("MainMenu"):
-        get_node("/root/MainMenu/CanvasLayer/CRTShader").visible = not config.get_value("video", "fullscreen_crt")
-    
-    get_node("/root/FPSCounter/RichTextLabel").visible = config.get_value("video", "fps_counter")
-    # TODO: make a mute function that mutes everything in a 'sounds' group
-
 
 func _on_Video_mouse_entered():
     $Hover.play()
@@ -188,6 +199,3 @@ func _on_Mute_pressed():
 
 func _on_Resolution_pressed():
     $Blip2.play()
-
-
-
