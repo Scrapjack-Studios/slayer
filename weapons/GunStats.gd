4ccef6d6df27c35a4cd850592f811d3d816ff816
PopeRigby <poperigby@tuta.io>
2020-07-31 12:08:58 -0700

    Cleanup bullet code

--- weapons/GunStats.gd
+++ weapons/GunStats.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
 export (PackedScene) var Bullet
-export (int) var dmg
 
 var is_automatic
 var is_burst_fire
 var is_semi_auto
 var shotgun
 var kickback = 0
+var damage
 
 var cool_down = 0.5
 #cool_down time for each shot, could also be reload time and effects the time in betwene shots of auto fire
@@ -15,25 +15,13 @@ var cool_down = 0.5
 var burst_ammount = 3
 #how many bullets are shot in one burst
 
-
-
-var bullet_size = Vector2(0.2,0.2)
-#default is 0.2
-
+var bullet_size = Vector2(0.2,0.2) #default is 0.2
 var bullet_speed = 3000
-#velocity of bullet
-
 var bullet_lifetime
-
 var weapon_sprite
-#Weapon sprite duh
-
 var weapon_size = Vector2(2,2)
-#weapon size
-
 var weapon_position = Vector2(0,0)
-#weapon location
-var shot = false    
+var shot = false
 
 var assault_sound
 var m1_sound
@@ -45,47 +33,72 @@ var shots_fired = 50
 var can_fire = true
 var ReloadTime = 2
 
+func spawn_projectile(pos, dir, dmg, speed, type, size, lifetime):
+    var new_projectile = Bullet.instance()
+    new_projectile.set_position(pos)
+    new_projectile.velocity = Vector2(speed, 0).rotated(dir)
+    new_projectile.damage = dmg
+    new_projectile.get_node("Sprite").animation = type
+    new_projectile.get_node("Sprite").set_rotation(dir)
+    new_projectile.get_node("Sprite").set_scale(size)
+    new_projectile.get_node("Explosion").set_scale(size)
+    new_projectile.add_to_group("bullets")
+    $Bullets.add_child(new_projectile)
+
 func _BulletPostition():
     if can_fire:
-        var b = Bullet.instance()
-        b.start_at(get_parent().get_node("Weapon_Sprite/Muzzle").global_position, get_parent().global_rotation,'black', dmg, bullet_lifetime, bullet_size, bullet_speed)
-        $Bullets.add_child(b)
         shots_fired -= 1
         shot = true
         if shots_fired == 0:
             can_fire = false   
-        if $RayCast2DKick.is_colliding():
-            get_parent().get_parent().Kickback(kickback)
-        
-        if shotgun:
-            var c = Bullet.instance()
-            var d = Bullet.instance()
-            var e = Bullet.instance()
-            var f = Bullet.instance()
-            var g = Bullet.instance()
-            var h = Bullet.instance()
-            c.start_at(get_parent().get_node("Weapon_Sprite/Muzzle").global_position, get_parent().global_rotation + 0.04,'blue', dmg, bullet_lifetime, bullet_size, bullet_speed)
-            d.start_at(get_parent().get_node("Weapon_Sprite/Muzzle").global_position, get_parent().global_rotation + 0.03,'blue', dmg, bullet_lifetime, bullet_size, bullet_speed)
-            e.start_at(get_parent().get_node("Weapon_Sprite/Muzzle").global_position, get_parent().global_rotation + 0.01,'blue', dmg, bullet_lifetime, bullet_size, bullet_speed)
-            f.start_at(get_parent().get_node("Weapon_Sprite/Muzzle").global_position, get_parent().global_rotation - 0.01,'blue', dmg, bullet_lifetime, bullet_size, bullet_speed)
-            g.start_at(get_parent().get_node("Weapon_Sprite/Muzzle").global_position, get_parent().global_rotation - 0.03,'blue', dmg, bullet_lifetime, bullet_size, bullet_speed)
-            h.start_at(get_parent().get_node("Weapon_Sprite/Muzzle").global_position, get_parent().global_rotation - 0.04,'blue', dmg, bullet_lifetime, bullet_size, bullet_speed)
-            $Bullets.add_child(c)
-            $Bullets.add_child(e)
-            $Bullets.add_child(g)
+#        if $RayCast2D.is_colliding():
+#            get_parent().get_parent().Kickback(kickback)
+        if not shotgun:
+            spawn_projectile(
+                    get_parent().get_node("Weapon_Sprite/Muzzle").global_position, 
+                    get_parent().get_node("Weapon_Sprite/Muzzle").global_position - get_global_mouse_position(), 
+                    damage, 
+                    bullet_speed, 
+                    'blue', 
+                    bullet_size, 
+                    bullet_lifetime
+                )
+        elif shotgun:
+            var rot_amount = 0.4
+            spawn_projectile(
+                    get_parent().get_node("Weapon_Sprite/Muzzle").global_position, 
+                    get_parent().global_rotation - 0.4,
+                    damage, 
+                    bullet_speed, 
+                    'blue', 
+                    bullet_size, 
+                    bullet_lifetime
+                )
+            spawn_projectile(
+                    get_parent().get_node("Weapon_Sprite/Muzzle").global_position, 
+                    get_parent().global_rotation,
+                    damage, 
+                    bullet_speed, 
+                    'blue', 
+                    bullet_size, 
+                    bullet_lifetime
+                )
+            spawn_projectile(
+                    get_parent().get_node("Weapon_Sprite/Muzzle").global_position, 
+                    get_parent().global_rotation + 0.4,
+                    damage, 
+                    bullet_speed, 
+                    'blue', 
+                    bullet_size, 
+                    bullet_lifetime
+                )
             var t = Timer.new()
             t.set_wait_time(0.03)
             t.set_one_shot(true)
             self.add_child(t)
             t.start()
             yield(t, "timeout")
-            $Bullets.add_child(d)
-            $Bullets.add_child(f)
-            $Bullets.add_child(h)
-            
-    
         if shot:
-        
             if assault_sound:
                 $Sounds/Assault_fire.play()
                 shot = false
@@ -98,7 +111,7 @@ func _BulletPostition():
             if super_shotgun_sound:
                 $Sounds/SuperShotgun_fire.play()
                 shot = false
-
+                
 func set_sprite():
     get_parent().get_node("Weapon_Sprite").texture = get_parent().get_node("GunStats").weapon_sprite
     get_parent().get_node("Weapon_Sprite").scale = get_parent().get_node("GunStats").weapon_size
