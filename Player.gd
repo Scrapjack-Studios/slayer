b9bdd8c5e98efe021db2487bdee74800e3f1eb0d
PopeRigby <poperigby@tuta.io>
2020-08-27 16:23:24 -0700

    Move reload out from under can_shoot

--- Player.gd
+++ Player.gd
@@ -80,23 +80,19 @@ func _ready():
 func _input(event: InputEvent) -> void:
     if is_network_master():
         
-        if can_shoot:
-            if Input.is_action_just_pressed("reload"):
+        if Input.is_action_just_pressed("reload"):
                 $WeaponMechanics.reload()
-            
-            
+        
+        if can_shoot:
             if event.is_action_pressed("gun_fire") and can_shoot and $Weapon/GunStats.is_semi_auto:
                 $WeaponMechanics.semi_auto()
                 GunTimer(false)
-                
             if event.is_action_pressed("gun_fire") and can_shoot and $Weapon/GunStats.shotgun:
                 $WeaponMechanics.shotgun()
                 GunTimer(false)
-                
             if event.is_action_pressed("gun_fire") and can_shoot and $Weapon/GunStats.is_burst_fire:
                 $WeaponMechanics.burst()
                 GunTimer(false)
-    
             
         if event.is_action_pressed("Graphook") and can_grapple:
                 rotation = 0
