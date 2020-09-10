d8cd8624e827d1e266c67495d4c72619c33374cd
PopeRigby <poperigby@tuta.io>
2020-09-09 17:44:28 -0700

    Add health tweening

--- obstacles/DamagingBlock.gd
+++ obstacles/DamagingBlock.gd
@@ -1,6 +1,5 @@
 extends StaticBody2D
 
-
 func _on_Area2D_body_entered(body):
     if body.is_in_group("Players"):
-        body.take_damage(100)
+        body.take_damage(10)
--- player/HealthBar.gd
+++ player/HealthBar.gd
@@ -1,4 +1,7 @@
 extends ProgressBar
 
 func on_Player_health_changed(health):
+    print(health)
     value = health
+    $UpdateTween.interpolate_property($HealthBarChange, "value", $HealthBarChange.value, health, 0.6, Tween.TRANS_SINE, Tween.EASE_OUT, 0.4)
+    $UpdateTween.start()
