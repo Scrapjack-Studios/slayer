4f7c98702118382d7fecb3d2745430c97f3370c6
PopeRigby <poperigby@tuta.io>
2020-09-10 14:20:32 -0700

    Add second damaging block

--- player/HealthBar.gd
+++ player/HealthBar.gd
@@ -1,7 +1,7 @@
 extends ProgressBar
 
 func on_Player_health_changed(health):
-    print(health)
     value = health
+    print(health)
     $UpdateTween.interpolate_property($HealthBarChange, "value", $HealthBarChange.value, health, 0.6, Tween.TRANS_SINE, Tween.EASE_OUT, 0.4)
     $UpdateTween.start()
