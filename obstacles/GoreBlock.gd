10aeb3c92e2ff89d45b67fd7566300d88fb78157
PopeRigby <poperigby@tuta.io>
2020-09-11 13:59:04 -0700

    Better blood particles

--- obstacles/GoreBlock.gd
+++ obstacles/GoreBlock.gd
@@ -1,14 +1,5 @@
 extends Node2D
 
 func hit():
-#    $AudioStreamPlayer.play(0.24)
-#    $RigidBodyParticles2D.set_emitting(true)
-#    var RTimer = Timer.new()
-#    RTimer.set_wait_time(1)
-#    RTimer.set_one_shot(true)
-#    self.add_child(RTimer)
-#    RTimer.start()
-#    yield(RTimer, "timeout")
-#    RTimer.queue_free()
-#    $RigidBodyParticles2D.set_emitting(false)
-    pass
+    $GibSound.play()
+    $BloodParticles.emitting = true
