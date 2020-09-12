bf7b7c5c85791168c4d18fdb06c9d8ac1a12f067
jujubunicorn <julianhenrywilson@gmail.com>
2020-09-12 15:08:59 -0700

    Change layer on player

--- GameController.gd
+++ GameController.gd
@@ -66,6 +66,8 @@ func spawn_peer(id):
     new_player.set_network_master(id)
     add_child(new_player)
     new_player.init(info.name, info.position)
+    new_player.set_collision_layer_bit(5, true)
+    new_player.set_collision_layer_bit(3, false)
     
 func on_Player_respawned():
     $CanvasLayer/HUD/HealthBar.value = player.health
