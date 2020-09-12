a641c3444f61f443060d565fbc770bd8a4a0b428
PopeRigby <poperigby@tuta.io>
2020-09-12 11:58:14 -0700

    Export damaging block damage

--- obstacles/DamagingBlock.gd
+++ obstacles/DamagingBlock.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+export (int) var damage 
+
 func _on_Area2D_body_entered(body):
     if body.is_in_group("Players"):
-        body.take_damage(100)
+        body.take_damage(damage)
