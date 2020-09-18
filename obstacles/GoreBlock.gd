86aa2e2a5ff6008974298b3ef199a2a24591a02d
PopeRigby <poperigby@tuta.io>
2020-09-18 14:25:05 -0700

    Make blood spurt in opposite direction of gun shot

--- obstacles/GoreBlock.gd
+++ obstacles/GoreBlock.gd
@@ -5,7 +5,7 @@ func hit(pos, rot):
     var blood_emitter = load("res://misc/BloodSpurt.tscn").instance()
     add_child(blood_emitter)
     blood_emitter.global_position = pos
-    blood_emitter.global_rotation = rot
+    blood_emitter.global_rotation = rot + PI
     blood_emitter.emitting = true
     $Timer.start(blood_emitter.lifetime)
     yield($Timer, "timeout")
