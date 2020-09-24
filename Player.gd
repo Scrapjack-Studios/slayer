969bdbe1cb5ce79ef14d758ed53d2d755f8063ab
PopeRigby <poperigby@mailbox.org>
2020-09-24 14:49:04 -0700

    Remove call deferred
    I'm not really sure why I had it in the first place

--- Player.gd
+++ Player.gd
@@ -413,7 +413,7 @@ sync func die():
     set_physics_process(false)
     can_shoot = false
     $Camera2D._set_current(false)
-    $CollisionShape2D.call_deferred("set_disabled", true)
+    $CollisionShape2D.disabled = true
     $BloodGore/GibSound.play()
     
 sync func respawn():
