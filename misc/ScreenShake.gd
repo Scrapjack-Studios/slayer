b392e893139c3dda64fd190b0f36641268dd2ed9
PopeRigby <poperigby@mailbox.org>
2020-09-27 15:00:15 -0700

    Add screenshake affect

--- misc/ScreenShake.gd
+++ misc/ScreenShake.gd
@@ -4,8 +4,17 @@ const TRANS = Tween.TRANS_SINE
 const EASE = Tween.EASE_IN_OUT
 
 var amplitude = 0
+var priority = 0
 
 onready var camera = get_parent()
+
+func start(duration, frequency = 15, amplitude = 16, priority = 0):
+    if priority >= self.priority:
+        self.priority = priority
+        self.amplitude = amplitude
+        
+        $Duration.start(duration)
+        $Frequency.start(1 / float(frequency))
 
 func new_shake():
     var rand = Vector2()
@@ -14,3 +23,16 @@ func new_shake():
     
     $ShakeTween.interpolate_property(camera, "offset", camera.offset, rand, $Frequency.wait_time, TRANS, EASE)
     $ShakeTween.start()
+
+func reset():
+    $ShakeTween.interpolate_property(camera, "offset", camera.offset, Vector2(), $Frequency.wait_time, TRANS, EASE)
+    $ShakeTween.start()
+    
+    priority = 0
+
+func _on_Frequency_timeout() -> void:
+    new_shake()
+
+func _on_Duration_timeout() -> void:
+    reset()
+    $Frequency.stop()
