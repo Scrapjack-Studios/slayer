a4da5eca51e83053128f2fba78f52f05af054811
PopeRigby <poperigby@mailbox.org>
2020-09-28 13:44:48 -0700

    Add very basic command parsing

--- menus/console/Console.gd
+++ menus/console/Console.gd
@@ -4,9 +4,25 @@ onready var scrollback_position = 0
 
 func _input(event: InputEvent) -> void:
     if event.is_action_pressed("ui_accept"):
-        var scrollback_line = load("res://menus/console/ScrollbackLine.tscn").instance()
-        scrollback_line.text = $Scrollback/Prompt/LineEdit.text
-        $Scrollback.add_child(scrollback_line)
-        $Scrollback.move_child(scrollback_line, scrollback_position)
-        scrollback_position += 1
+        update_scrollback($Scrollback/Prompt/LineEdit.text)
+        run_command($Scrollback/Prompt/LineEdit.text)
         $Scrollback/Prompt/LineEdit.clear()
+        
+func update_scrollback(command):
+    var scrollback_line = load("res://menus/console/ScrollbackLine.tscn").instance()
+    scrollback_line.text = " $ " + command
+    $Scrollback.add_child(scrollback_line)
+    $Scrollback.move_child(scrollback_line, scrollback_position)
+    scrollback_position += 1
+    
+func console_print(statement):
+    var scrollback_line = load("res://menus/console/ScrollbackLine.tscn").instance()
+    scrollback_line.text = str(statement)
+    $Scrollback.add_child(scrollback_line)
+    $Scrollback.move_child(scrollback_line, scrollback_position)
+    scrollback_position += 1
+        
+func run_command(command):
+    if command == "help":
+        console_print("no")    
+
