590001f132f26cb0d07a2e527e32729d169682a9
PopeRigby <poperigby@mailbox.org>
2020-10-03 13:16:52 -0700

    Add arguments and focus grabbing

--- menus/console/Console.gd
+++ menus/console/Console.gd
@@ -2,9 +2,15 @@ extends Control
 
 onready var scrollback_position = 0
 
+var admin = true
+
 func _input(event: InputEvent) -> void:
     if event.is_action_pressed("toggle_console"):
-        visible = not visible
+        if visible:
+            visible = false
+        elif not visible:
+            visible = true
+            $Scrollback/Prompt/LineEdit.call_deferred("grab_focus")
     if event.is_action_pressed("ui_accept"):
         update_scrollback($Scrollback/Prompt/LineEdit.text)
         run_command($Scrollback/Prompt/LineEdit.text)
@@ -24,10 +30,23 @@ func console_print(statement):
     $Scrollback.move_child(scrollback_line, scrollback_position)
     scrollback_position += 1
         
-func run_command(command):
+func run_command(command, argument=""):
     if command == "help":
         Help()
+    if command == "kill":
+        if $"/root".has_node("GameController"):
+            if not argument:
+                var victim = $"/root/GameController".get_node(str(get_tree().get_network_unique_id()))
+                victim.take_damage(100)
+        else:
+            console_print("You must be in a game to run this command")
+        
         
 func Help():
     console_print("Figure it out yourself")
+    
+func KillSelf():
+    
+    $"/root"
+    
 
