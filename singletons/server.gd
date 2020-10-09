c0800b12cab7761b7c62888f4ea445f3f38894d1
PopeRigby <poperigby@mailbox.org>
2020-10-08 18:31:21 -0700

    Add some import sigs and vars to server.gd

--- singletons/server.gd
+++ singletons/server.gd
@@ -4,6 +4,20 @@ var network = NetworkedMultiplayerENet.new()
 var ip = "127.0.0.1"
 var port = 4000
 
+var players = { }
+var start_position = Vector2(360,180)
+var self_data = {name = '', position = Vector2(), received_disconnect=false}
+var disconnected_player_info
+var connected_player_info
+var connected_player
+var disconnected
+
+signal player_disconnected
+signal server_disconnected
+signal player_connection_completed
+signal player_disconnection_completed
+signal server_stopped
+
 func connect_to_server(ip, port):
 	network.create_client(ip, port)
 	get_tree().set_network_peer(network)
