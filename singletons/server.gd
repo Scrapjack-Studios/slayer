5cd5fae33c4dbdefb286ec06fa69b8ef854368b3
PopeRigby <poperigby@mailbox.org>
2020-10-11 11:19:42 -0700

    Relay start pos on player connect

--- singletons/server.gd
+++ singletons/server.gd
@@ -3,6 +3,7 @@ extends Node
 var network = NetworkedMultiplayerENet.new()
 var players = {}
 var self_data = {username = '', position = Vector2(), received_disconnect=false}
+var start_position
 
 func connect_to_server(ip, port, username):
 	self_data.username = username
@@ -14,11 +15,15 @@ remote func fetch_player_info():
 	rpc_id(1, "get_player_info", get_tree().get_network_unique_id(), self_data)
 
 remote func get_map(map):
+	# Why does this need to be in Global?
 	Global.map = map
 	get_tree().change_scene("res://GameController.tscn")
 
 remote func get_players_list(players_list):
 	players = players_list
+
+remote func get_start_position(start_pos):
+	start_position = start_pos
 
 remote func kicked(reason):
 	Global.kick_reason = reason 
@@ -26,17 +31,3 @@ remote func kicked(reason):
 	get_tree().change_scene("res://MainMenu.tscn")
 	emit_signal("player_disconnection_completed", get_tree().get_network_unique_id())
 	get_tree().set_network_peer(null)
-
-#remote func receive_players_info(id, info):
-#	players[id] = info
-#	if connected_player in players:
-#		connected_player_info = players[connected_player]
-#		emit_signal("player_connection_completed")
-#	print(players)
-
-#remote func _request_players(request_from_id):
-#	if get_tree().is_network_server():
-#		rpc_id(request_from_id, '_send_players', players)
-#
-#remote func _send_players(players_array):
-#	players = players_array
