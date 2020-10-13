5bd099b1e7408e84f396e2966841bd0f0a69eb92
Julian Wilson <julian@pop-os.localdomain>
2020-10-13 14:18:56 -0700

    Better hit marker sprite

--- weapons/FireSound.gd
+++ weapons/FireSound.gd
@@ -5,9 +5,9 @@ var shotgun
 var pistol
 
 func activate():
-    if assault:
-        stream = load("res://assets/sfx/weapons/AssaultRifle.wav")
-    if pistol:
-        stream = load("res://assets/sfx/weapons/Pistol.wav")
-    if shotgun:
-        stream = load("res://assets/sfx/weapons/Combat_Shotgun.wav")
+	if assault:
+		stream = load("res://assets/sfx/weapons/AssaultRifle.wav")
+	if pistol:
+		stream = load("res://assets/sfx/weapons/Pistol.wav")
+	if shotgun:
+		stream = load("res://assets/sfx/weapons/Combat_Shotgun.wav")
